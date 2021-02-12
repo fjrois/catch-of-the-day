fe6199e93d46e737382158473a0fe7b726d09224
fjrois <fjroissiso@gmail.com>
2021-02-12 12:05:14 -0800

    Added useless button to home screen

--- src/components/catchOfTheDay/StorePicker.js
+++ src/components/catchOfTheDay/StorePicker.js
@@ -28,18 +28,21 @@ class StorePicker extends React.Component {
   };
   render() {
     return (
-      <form className="store-selector" onSubmit={this.goToStore}>
-        <h2>Please enter a store</h2>
-        <input
-          type="text"
-          required
-          placeholder="Store Name"
-          defaultValue={this.state.initialStoreId}
-          onChange={this.handleInputChange}
-          name="storeId"
-        />
-        <button type="submit">Visit Store</button>
-      </form>
+      <>
+        <form className="store-selector" onSubmit={this.goToStore}>
+          <h2>Please enter a store!!</h2>
+          <input
+            type="text"
+            required
+            placeholder="Store Name"
+            defaultValue={this.state.initialStoreId}
+            onChange={this.handleInputChange}
+            name="storeId"
+          />
+          <button type="submit">Visit Store</button>
+        </form>
+        <button onClick={() => alert('HI!')}>Useless button</button>
+      </>
     );
   }
 }
